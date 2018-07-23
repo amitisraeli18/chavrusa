4c3bfeff606a734862771d1c62a819f3a200db19
Amit Israeli <amitisraeli18@apples-MacBook-Pro.local>
2018-07-22 20:39:34 -0400

    ok

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
   var fs = require('fs');
   var server=require("http").createServer(start);
   var io = require("socket.io")(server);
-  var shtikles = [];
+  var words = [];
+  var curID, curWord;
   var port = process.env.OPENSHIFT_NODEJS_PORT || 8080, 
-        id = process.env.OPENSHIFT_NODEJS_IP || "10.0.0.36";
+        id = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+
+  function saveWords() {
+    fs.writeFile("./data/words.txt", JSON.stringify(words), function(err) {
+        if(err) {
+            return console.log(err);
+        } 
+
+        console.log("saved it!");
+        io.emit("word added", {idx:curID,word:curWord});
+    })
+  }
 
   function start(r,res) {
-    console.log("Hello World!", process.env); 
+
+    console.log("Hello World!"); 
     res.writeHead(200, {'Content-Type':'text/html'});
     fs.readFile("chavruta.html", function(error, data) {
+        console.log("READ");
         if(error) {
             console.log(error);
         } else {
@@ -17,18 +31,26 @@
         res.end();
     });
 
-    fs.readFile("maamers/maamer1.txt", "utf8", (error, data) => {
-        if(error) {
-            console.log(error);
-        } else {
-            io.emit("maamer loaded", data);
-        }
-    });
+    
     io.on("connection", function(socket) {
+        fs.readFile("maamers/maamer1.txt", "utf8", (error, data) => {
+            if(error) {
+                console.log(error);
+            } else {
+                io.emit("maamer loaded", data);
+            }
+        });
         console.log("yay a connection!");
-       /* io.on("word list", function(data) {
-            shtikles = data;
-        });*/
+        socket.on("new word", function(obj) {
+            if(obj && obj.word) {
+                words.push(obj.word);
+                curID = words.length-1;
+                curWord = obj.word;
+                console.log("YAY", obj.id);
+                saveWords();
+            }
+        });
+
         socket.on("word clicked", function(idx) {
             console.log(idx);
             io.emit("selected", idx);
